4f13de6636687d76e2a3bb98ceb7aa60ccef67e4
Sharon Mwenda <smkathambi@gmail.com>
2022-06-12 19:04:32 +0300

    Done.

--- index.js
+++ index.js
@@ -12,5 +12,11 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(title => {
+    return title.split(" ").map(word => {
+      return word[0].toUpperCase()+ word.substring(1);
+      }).join(" ");
+    //console.log(titleCased)
+  })
 }
+console.log(titleCased());
